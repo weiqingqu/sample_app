ac300c67133b4750911491a11b6501adcae00642
freshlhy <freshlhy@gmail.com>
2012-08-30 11:35:09 +0800

    Finish static pages

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -6,6 +6,12 @@ describe "Static pages" do
       visit '/static_pages/home'
       page.should have_content('Sample App')
     end
+
+    it "should have the right title 'Home'" do
+      visit '/static_pages/home'
+      page.should have_selector('title', 
+                  :text => "Ruby on Rails Tutorial Sample App | Home")
+    end
   end
 
   describe "Help page" do
@@ -13,13 +19,24 @@ describe "Static pages" do
       visit '/static_pages/help'
       page.should have_content('Help')
     end
-  end
 
-  describe "About page" do
-    it "should have the content 'About Us" do
-      visit '/static_pages/about'
-      page.should have_content('About Us')
+    it "should have the right title 'Help'" do
+      visit '/static_pages/help'
+      page.should have_selector('title', 
+                  :text => "Ruby on Rails Tutorial Sample App | Help")
     end
   end
 
+  describe "About page" do
+    it "should have the content 'About Us'" do
+      visit '/static_pages/about'
+      page.should have_content('About Us')
+    end
+
+    it "should have the right title 'About Us'" do
+      visit '/static_pages/about'
+      page.should have_selector('title', 
+                  :text => "Ruby on Rails Tutorial Sample App | About Us")
+    end
+  end
 end
