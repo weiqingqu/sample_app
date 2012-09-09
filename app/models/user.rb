38b688fe4a004cf70b19b81ce33b7724fb21158e
liuhuyu <735077491@qq.com>
2012-09-10 06:25:05 +0800

    modeling users v2 @home

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,8 +12,11 @@
 class User < ActiveRecord::Base
   attr_accessible :email, :name
 
+  before_save { |user| user.email = email.downcase }
+
   validates :name, presence: true, Length: { maximum: 50 }
-  validates :email,presence: true
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
 
 
 end
