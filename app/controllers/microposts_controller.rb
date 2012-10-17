24d1f3501ac08105515f06278c2fe16a2d92747d
freshlhy <freshlhy@gmail.com>
2012-10-18 00:06:44 +0800

    user micropost v3

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -5,6 +5,14 @@ class MicropostsController < ApplicationController
   end
 
   def create
+    @micropost = current_user.microposts.build(params[:micropost])
+    if @micropost.save
+      flash[:success] = "Micropost created!"
+      redirect_to root_url
+    else
+      @feed_items = []
+      render 'static_pages/home'
+    end
   end
 
   def destroy
